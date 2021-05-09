a821b42fe736dd4c2156153e5080e6befc36d8d0
fenil25 <fdoshi@umass.edu>
2021-05-09 16:45:36 -0400

    Solved indentation erros

--- train.py
+++ train.py
@@ -75,13 +75,13 @@ def train(net, dataset, test_dataset, device, args):
             net.train()
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Training the model')
-	parser.add_argument("--voxel_size", type=float, default=0.05, help="More the vocel size, lesser the resolution")
-	parser.add_argument("--max_steps", type=int, default=15000, help="Number of training steps")
-	parser.add_argument("--val_freq", type=int, default=1000, help="Frequency of testing on validation set")
-	parser.add_argument("--batch_size", default=32, type=int, help="Batch size of dataloader")
-	parser.add_argument("--lr", default=1e-1, type=float, help="Learning rate of SGD optimizer")
-	parser.add_argument("--weight_decay", type=float, default=1e-4, help="Weight decay for optimizer")
+    parser = argparse.ArgumentParser(description='Training the model')
+    parser.add_argument("--voxel_size", type=float, default=0.05, help="More the vocel size, lesser the resolution")
+    parser.add_argument("--max_steps", type=int, default=15000, help="Number of training steps")
+    parser.add_argument("--val_freq", type=int, default=1000, help="Frequency of testing on validation set")
+    parser.add_argument("--batch_size", default=32, type=int, help="Batch size of dataloader")
+    parser.add_argument("--lr", default=1e-1, type=float, help="Learning rate of SGD optimizer")
+    parser.add_argument("--weight_decay", type=float, default=1e-4, help="Weight decay for optimizer")
 
     parser.add_argument("--translation", default=0.2, type=float, help="Translations applied to points in a unit sphere")
     parser.add_argument("--axis_rotations", default=3, type=int, help="Number of axis along which rotations need to be performed")
@@ -89,20 +89,20 @@ if __name__ == '__main__':
     parser.add_argument("--jitter", default=0.02, type=float, help="Random Jitter that is added to all points")
     parser.add_argument("--scale_factor", default=0.1, type=float, help="Deviation in scaling factor to all points")
 
-	parser.add_argument("--path_to_dataset", type=str, help="Path to the training and testing points directory", required=True)
-	parser.add_argument("--store_weights", type=str, default="modelnet.pth", help="Location where weights would be stored")
+    parser.add_argument("--path_to_dataset", type=str, help="Path to the training and testing points directory", required=True)
+    parser.add_argument("--store_weights", type=str, default="modelnet.pth", help="Location where weights would be stored")
 
-	parser.add_argument("--num_workers", type=int, default=2)
-	parser.add_argument("--stat_freq", type=int, default=100)
-	parser.add_argument("--seed", type=int, default=777)
+    parser.add_argument("--num_workers", type=int, default=2)
+    parser.add_argument("--stat_freq", type=int, default=100)
+    parser.add_argument("--seed", type=int, default=777)
 
-	parser.add_argument("--load_pretrained", action="store_true", help="Load the pretrained weights and start training from there")
-	parser.add_argument("--path_weights", type=str, default="modelnet.pth", help="Path to trained weights for resuming training")
+    parser.add_argument("--load_pretrained", action="store_true", help="Load the pretrained weights and start training from there")
+    parser.add_argument("--path_weights", type=str, default="modelnet.pth", help="Path to trained weights for resuming training")
 
-	args = parser.parse_args()
-	print(args)
+    args = parser.parse_args()
+    print(args)
 
-	ctr = CoordinateTransformation(
+    ctr = CoordinateTransformation(
         scale_range=(1-args.scale_factor, 1+args.scale_factor), 
         rot_range = {"X": (-args.degree_rot, args.degree_rot), 
             "Y": (-args.degree_rot, args.degree_rot), 
@@ -111,13 +111,13 @@ if __name__ == '__main__':
         rotations=args.axis_rotations
     )
 
-	print("Loading the ModelNet40 dataset...")
-	train_dataset = ModelNet40(args.path_to_dataset, phase = "train", transform = ctr)
-	test_dataset = ModelNet40(args.path_to_dataset, phase = "test", transform = None)
-	print("Dataset successfully loaded")
+    print("Loading the ModelNet40 dataset...")
+    train_dataset = ModelNet40(args.path_to_dataset, phase = "train", transform = ctr)
+    test_dataset = ModelNet40(args.path_to_dataset, phase = "test", transform = None)
+    print("Dataset successfully loaded")
 
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-	net = PoxelNet(in_channel = 3, out_channel = 40, embedding_channel = 1024).to(device)
+    net = PoxelNet(in_channel = 3, out_channel = 40, embedding_channel = 1024).to(device)
 
-	train(net, train_dataset, test_dataset, device, args)
+    train(net, train_dataset, test_dataset, device, args)
